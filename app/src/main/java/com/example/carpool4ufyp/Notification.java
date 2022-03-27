f83fa4fcd8651d1f4a3fd4d2134ddf2485bffbc3
laurenkearney00 <c18330401@mytudublin.ie>
2022-03-27 14:42:23 +0100

    Updates

---
+++ app/src/main/java/com/example/carpool4ufyp/Notification.java
@@ -0,0 +1,53 @@
+package com.example.carpool4ufyp;
+
+public class Notification {
+    private String message;
+    private String sender;
+    private String receiver;
+    private String timestamp;
+
+    public Notification(){
+
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public String getSender() {
+        return sender;
+    }
+
+    public String getReceiver() {
+        return receiver;
+    }
+
+    public void setSender(String sender) {
+        this.sender = sender;
+    }
+
+    public String getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(String timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public void setReceiver(String receiver) {
+        this.receiver = receiver;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public Notification(String message, String receiver, String sender, String timestamp) {
+        this.message = message;
+        this.receiver = receiver;
+        this.sender = sender;
+        this.timestamp = timestamp;
+    }
+
+
+}
